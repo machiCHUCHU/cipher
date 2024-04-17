f048dad0b174eb8d9e83faf128351a209e61399a
DenverDeoGorospe <gorospedenverdeo@gmail.com>
2024-04-18 00:00:13 +0800

    updated

--- get-out.php
+++ get-out.php
@@ -10,8 +10,8 @@ $loginTimeRangesAM = array(
 );
 
 $loginTimeRangesPM = array(
-    'start' => '23:00:00',
-    'end' => '24:00:00'
+    'start' => '16:00:00',
+    'end' => '17:00:00'
 );
 
 if (isset($_GET['stud_id'])) {
@@ -62,7 +62,7 @@ if (isset($_GET['stud_id'])) {
             echo "Student not found";
         }
     } else {
-        echo "Log IN not allowed.";
+        echo "Log OUT not allowed.";
     }
 } else {
     echo "Invalid request";
