218b670cb31228e20d92c96460f710dd1f1f6308
DenverDeoGorospe <gorospedenverdeo@gmail.com>
2024-04-17 21:58:26 +0800

    time

--- get.php
+++ get.php
@@ -2,6 +2,7 @@
 require("dbconnection.php");
 
 
+date_default_timezone_set('Asia/Manila');
 if (isset($_GET['stud_id'])) {
     $studentId = $_GET['stud_id'];
 
@@ -16,9 +17,10 @@ if (isset($_GET['stud_id'])) {
         $id=$row["id"];
         $studentInfo = "Name: " . $row['name'] . "\n";
         $studentInfo .= "Student ID: " . $row['stud_id'] . "\n";
-        $studentInfo .= "Section: " . $row['section'];
+        $studentInfo .= "Section: " . $row['section'] . "\n";
+        $studentInfo .= "Time_IN: " . date('h:i:s A');
         
-        $currentTime = date('H:i:s');
+        $currentTime = date('h:i:s A');
         $currentDate = date('Y-m-d');
         $query = "INSERT INTO tblattendance (userid, dated, in_AM) VALUES (:id, :dated, :in_AM)";
         $stmt = $conn->prepare($query);
